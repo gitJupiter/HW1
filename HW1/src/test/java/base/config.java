36b3ee798a6314c18ffd63dcf7a9c074fda963c7
gitJupiter <kmilon04@gmail.com>
2023-10-09 22:27:22 -0400

    setup browser in config

--- HW1/src/test/java/base/config.java
+++ HW1/src/test/java/base/config.java
@@ -1,8 +1,28 @@
 package base;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.safari.SafariDriver;
+
+import java.time.Duration;
 
 public class config {
 
     public static WebDriver bk;
+
+public static WebDriver browser(String drivertype){
+
+    if(drivertype.equalsIgnoreCase('ch')){
+        bk= new ChromeDriver();
+    } else if (drivertype.equalsIgnoreCase('FF')) {
+        bk= new FirefoxDriver();
+    } else if (drivertype.equalsIgnoreCase('safari')) {
+        bk=new SafariDriver();
+    }
+    bk.manage().window().maximize();
+    bk.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+    return bk;
+}
+
 }
